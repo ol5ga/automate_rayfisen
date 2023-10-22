eceda530f6401e17eb55b2634eddba20df57d5bb
shata <shatalova.o.s@yandex.ru>
2023-10-22 22:03:34 +0300

    db create

--- src/main/java/org/example/model/Sock.java
+++ src/main/java/org/example/model/Sock.java
@@ -1,9 +1,18 @@
 package org.example.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "socks")
 public class Sock {
 
+    @Id
+    @GeneratedValue
     int id;
+    @Column(name = "color", nullable = false)
     String color;
+    @Column(name="cotton_part")
     int cottonPart;
+    @Column(name="quantity")
     int quantity;
 }
